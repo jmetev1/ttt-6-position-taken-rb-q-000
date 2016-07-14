7ec6be35b9dd610e2ae5ba0dceaf0c36305eba3f
Jacques Metevier <jmetevier@gmail.com>
2016-07-14 16:18:44 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+#board=Array.new(9," ")
+def position_taken?(board, index)
+  if board[index]==" "
+    false
+  elsif board[index]==""
+    false
+  elsif board[index]==nil
+    false
+  else
+    true
+  end
+end
+board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
+output=position_taken?(board,0)
